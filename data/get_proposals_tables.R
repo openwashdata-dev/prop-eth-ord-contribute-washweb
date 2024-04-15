8c7873ee6d6ad0100e11c11613a8f36058529f92
Lars Schöbitz <lschoebitz@ethz.ch>
2024-04-15 16:45:38 +0200

    submitted proposal

--- data/get_proposals_tables.R
+++ data/get_proposals_tables.R
@@ -11,6 +11,37 @@ library(dplyr)
 
 # script ------------------------------------------------------------------
 
-read_sheet("https://docs.google.com/spreadsheets/d/1xMlMepWoq4lTRRP89x8A1A-1Bkqkn7hjslvpJ5XPXU8/edit#gid=0") |> 
-    janitor::clean_names() |>
-    write_csv(here::here("data/tbl-04-wp-activities.csv"))
+# script ------------------------------------------------------------------
+
+## tbl-01-work-packages --------------------------------------------------
+
+## Note: Update the Google Sheet ID with your copy of the table
+
+read_sheet("1d-WzPHFpYGLQcodclDI7L13yYnp-MU_aOtzt-vctvf0") |> 
+    write_csv(here::here("data/tables/tbl-01-work-packages.csv"))
+
+
+## tbl-02-wp-activities-research-questions.csv ------------------------------
+
+## Note: Update the Google Sheet ID with your copy of the table
+
+read_sheet("1xMlMepWoq4lTRRP89x8A1A-1Bkqkn7hjslvpJ5XPXU8") |> 
+    write_csv(here::here("data/tables/tbl-02-wp-activities-research-questions.csv"))
+
+
+## tbl-03-eth-ord-budget-templa-----------------------------------------------
+
+## copy/paste the final budget table into the DOCX template
+## Note: Update the Google Sheet ID with your copy of the table
+
+read_sheet("1p77mMyCWZ3r8N73ZjYB_u038cwsdsMWs6rMjDVwBW_c") |> 
+    write_csv(here::here("data/tables/tbl-03-eth-ord-budget-template.csv"))
+
+## tbl-04-budget-justification -----------------------------------------------
+
+## copy/paste the final budget table into the DOCX template
+## Note: Update the Google Sheet ID with your copy of the table
+
+read_sheet("1OKAFWg4PyPp7_BikkQOh9QQ8t982iOTHsB1c5A6SoqU") |> 
+    write_csv(here::here("data/tables/tbl-04-budget-justification.csv"))
+
